ead299439b20fc60394a137c612965ed9761b320
deca <62450933+decaprime@users.noreply.github.com>
2021-09-26 00:05:36 -0700

    ✨ Support either directly exported and editor exported recordings. Other style/refactorings: - Improved instructions - Clarified margins as Warnings - Output defaults to generating if ladder macro script - Enable nullable

--- RecordingParser.cs
+++ RecordingParser.cs
@@ -2,13 +2,45 @@
 
 public class RecordingParser
 {
-	public static (string code, string error) Parse(string bunchOfLines)
+	private record Recording(Vector2 Size, List<Vector2> Points);
+
+	public record Result(UniversalCoordinates? Output = null, string? Warning = null, string? Error = null);
+
+	public record UniversalCoordinates(List<UniversalCoord> Coordinates)
+	{
+		public string AsScript => string.Join(Environment.NewLine, Coordinates);
+
+		public string AsScriptWithMacro => $@"; Generated with https://universal-coordinator.deca.gg
+; Use by setting RENAME_ME_INDEX and calling this script or jumping to the label
+:global int RENAME_ME_INDEX
+:local int _ci
+_ci = RENAME_ME_INDEX
+
+#rvec(wx,hx,wy,hy) vec(width.d()* {{wx}}+height.d()* {{hx}},width.d()* {{wy}}+height.d()* {{hy}})
+
+universal_click:
+	click(\
+{string.Join(Environment.NewLine, Coordinates.Select((c, i) => $"\t\tif(_ci == {i,2}, {c.RVec}, \\"))}
+    		vec(0.0,0.0){new(')', Coordinates.Count)} \
+	)
+";
+	}
+
+	public record UniversalCoord(Vector2 W, Vector2 H, Vector2 Error)
+	{
+		public string RVec => $"{{rvec({W.X,6:F3}, {H.X,6:F3}, {W.Y,6:F3}, {H.Y,6:F3})}}";
+
+		public override string ToString() =>
+			$"click(vec(width.d()* {W.X,6:F3}+height.d()* {H.X,6:F3},width.d()* {W.Y,6:F3}+height.d()* {H.Y,6:F3}))";
+	}
+
+	public static Result Parse(string bunchOfLines)
 	{
 		if (string.IsNullOrWhiteSpace(bunchOfLines))
-			return (null, error: "Input empty");
+			return new() { Error = "Input empty" };
 		IEnumerable<string> recordings = bunchOfLines.Split(Environment.NewLine).Where(s => !string.IsNullOrWhiteSpace(s));
 
-		if (recordings.Count() < 3) return (null, error: "Requires 3 or more entries");
+		if (recordings.Count() < 3) return new() { Error = "Requires 3 or more entries" };
 
 		try
 		{
@@ -18,26 +50,27 @@ public class RecordingParser
 		}
 		catch (Exception)
 		{
-			return (null, error: "Failed to parse input as list of base64 recordings");
+			// TODO should suggest reporting github issue if it's reasonably sized Base64 at least, maybe missed compatability
+			return new() { Error = "Failed to parse input as list of base64 recordings" };
 		}
 	}
 
-	static (string code, string error) DetermineCoords(IEnumerable<(Vector2 size, List<Vector2> points)> entries)
+	private static Result DetermineCoords(IEnumerable<Recording> entries)
 	{
-		string result = "", errors = "";
-		var numPoints = entries.First().points.Count;
-
-		if (entries.Any(r => r.points.Count != numPoints))
-			return (code: null, error: "Not all recordings have the same number of points");
+		string? warning = null;
+		var numPoints = entries.First().Points.Count;
+		var coordinates = new List<UniversalCoord>();
+		if (entries.Any(r => r.Points.Count != numPoints))
+			return new() { Error = "Not all recordings have the same number of points" };
 
 		for (var i = 0; i < numPoints; i++)
 		{
-			Vector2 a1 = default, a2 = default, b = default, c = default, sol1 = default, sol2 = default, err = default;
-			float ratio = 0, determinant = 0;
+			Vector2 a1 = default, a2 = default, b = default, c = default;
+			float ratio = 0;
 			foreach (var recording in entries)
 			{
-				var size = recording.size;
-				var point = recording.points[i];
+				var size = recording.Size;
+				var point = recording.Points[i];
 
 				ratio += size.X * size.Y;
 				c += size * size;
@@ -47,49 +80,64 @@ public class RecordingParser
 				b += point * point;
 			}
 
-			determinant = (c.X * c.Y) - (ratio * ratio);
-			sol1 = ((c.Y * a1) - (ratio * a2)) / determinant;
-			sol2 = ((c.X * a2) - (ratio * a1)) / determinant;
-			err = ((b - (a1 * sol1)) - (a2 * sol2)) / (i + 1);
+			var determinant = (c.X * c.Y) - (ratio * ratio);
+			var w = ((c.Y * a1) - (ratio * a2)) / determinant;
+			var h = ((c.X * a2) - (ratio * a1)) / determinant;
+			var errSq = ((b - (a1 * w)) - (a2 * h)) / (i + 1);
+			var err = new Vector2(MathF.Sqrt(errSq.X), MathF.Sqrt(errSq.Y));
 
-			result += $"click(vec(width.d()*{sol1.X:F3}+height.d()*{sol2.X:F3},width.d()*{sol1.Y:F3}+height.d()*{sol2.Y:F3})){Environment.NewLine}";
-			if (err.X > 8.0) errors += $"Point {i + 1} X error = {err.X:F2}{Environment.NewLine}";
-			if (err.Y > 8.0) errors += $"Point {i + 1} Y error = {err.Y:F2}{Environment.NewLine}";
+			if (err.X > 8) warning = (warning ?? "") + $"Point {i + 1}'s X margin of error ± {err.X:F2}px{Environment.NewLine}";
+			if (err.Y > 8) warning = (warning ?? "") + $"Point {i + 1}'s Y margin of error ± {err.Y:F2}px{Environment.NewLine}";
+			coordinates.Add(new UniversalCoord(w, h, err));
 		}
-		return (result, errors);
+
+		return new Result(new UniversalCoordinates(coordinates), warning);
 	}
 
-	static (Vector2 size, List<Vector2> points) ParseRecording(string base64)
+	static Recording ParseRecording(string base64)
 	{
-		var s = Convert.FromBase64String(base64);
-		using var ms = new MemoryStream(s);
-		using var br = new System.IO.BinaryReader(ms);
+		var buffer = Convert.FromBase64String(base64);
+		using var memStream = new MemoryStream(buffer);
+		using var reader = new BinaryReader(memStream);
 
-		var name = br.ReadString();
-		br.ReadInt32();
-		br.ReadInt32();
-		var lines = br.ReadInt32();
+		var scriptName = reader.ReadString();
+		reader.ReadInt32();
+		reader.ReadInt32();
+		var numLines = reader.ReadInt32();
 
 		Vector2 readCoord(BinaryReader br)
 		{
-			br.ReadString(); // generic.click
-			br.ReadString(); //vec.fromCoords
-			br.ReadString(); // constants
-			br.ReadByte(); // 3 (double)
-			var x = br.ReadDouble(); // x coord
-			br.ReadString(); // constant
-			br.ReadByte(); // 3 (double)
-			var y = br.ReadDouble(); // y coord
-			return new Vector2((float)x, (float)y); // technically losing precision here, I don't think it matters.
+			var genClick = br.ReadString();         // generic.click
+			var nextOp = br.ReadString();           //vec.fromCoords or constant
+			if (nextOp == "vec.fromCoords")
+			{
+				var constants = br.ReadString();    // constant
+				var constType = br.ReadByte();      // 3 (double)
+				var x = br.ReadDouble();            // x coord
+				var constants2 = br.ReadString();   // constant
+				var constType2 = br.ReadByte();     // 3 (double)
+				var y = br.ReadDouble();            // y coord
+
+				return new Vector2((float)x, (float)y);
+			}
+			else if (nextOp == "constant")
+			{
+				var constType = br.ReadByte();      // 5 (vec)
+				var x = br.ReadSingle();            // x
+				var y = br.ReadSingle();            // y
+
+				return new Vector2(x, y);
+			}
+			else throw new InvalidDataException($"Expected to encounter vec.FromCoords or constant but was parsed as {nextOp}");
 		}
 
 		List<Vector2> points = new();
-		var size = readCoord(br); // should be first
-		for (var i = 1; i < lines; i++)
+		var size = readCoord(reader); // should be first
+		for (var i = 1; i < numLines; i++)
 		{
-			points.Add(readCoord(br));
+			points.Add(readCoord(reader));
 		}
 
-		return (size, points);
+		return new Recording(size, points);
 	}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
-    purge: {
-        enabled: true,
-        content: [
-            './**/*.html',
-            './**/*.razor'
-        ],
-    },
-    darkMode: "class",
-    variants: {
-        extend: {opacity:['disabled']},
-    },
-    plugins: [require('@tailwindcss/forms')],
+	purge: {
+		enabled: true,
+		content: [
+			'./**/*.html',
+			'./**/*.razor'
+		],
+	},
+	darkMode: "class",
+	variants: {
+		extend: { opacity: ['disabled'], borderWidth: ['hover']},
+	},
+	plugins: [require('@tailwindcss/forms')],
 }
